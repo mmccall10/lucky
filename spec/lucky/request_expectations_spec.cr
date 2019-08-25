13291248b0da30bb0f69e039f40c9ef280ca2e3b
Paul Smith <paulcsmith0218@gmail.com>
2019-08-25 11:47:54 -0400

    Get some assertions working

--- spec/lucky/request_expectations_spec.cr
+++ spec/lucky/request_expectations_spec.cr
@@ -1,13 +1,30 @@
 require "../spec_helper"
 
+include Lucky::RequestExpectations
+
 describe Lucky::RequestExpectations do
-  it "fails if the response is not JSON" do
+  it "fails if the status is incorrect" do
+    failure_message = "Expected status of 200. Instead got 201."
+    expect_raises Spec::AssertionFailed, failure_message do
+      response = build_response(201)
+      response.should send_json(200)
+    end
   end
 
-  it "fails if the status is incorrect" do
+  it "fails if the JSON is not parseable" do
+    failure_message = "Response body is not valid JSON."
+    expect_raises Spec::AssertionFailed, failure_message do
+      response = build_response(200, body: "this is not json")
+      response.should send_json(200)
+    end
   end
 
   it "fails if an expected key is missing" do
+    failure_message = /Expected response to have JSON key "foo"/
+    expect_raises Spec::AssertionFailed, failure_message do
+      response = build_response(200, body: {foo: "bar"}.to_json)
+      response.should send_json(200, admin: true)
+    end
   end
 
   it "fails and provides helpful error if key is missing and similar key is found" do
@@ -18,4 +35,11 @@ describe Lucky::RequestExpectations do
 
   it "passes if the response has matching keys/values and ignores extra keys/values" do
   end
+end
+
+private def build_response(status : Int32, body : String = "{}")
+  HTTP::Client::Response.new(
+    status_code: status,
+    body: body
+  )
 end
--- src/lucky/request_expectations.cr
+++ src/lucky/request_expectations.cr
@@ -1,33 +1,53 @@
 module Lucky::RequestExpectations
-  def send_json(status, **expected_response)
-    SendJsonExpectation.new(status, expected_response.to_json)
+  def send_json(status, **expected)
+    SendJsonExpectation.new(status, expected.to_json)
   end
 
   struct SendJsonExpectation
-    def initialize(@status : Int32, @expected_json : String)
+    private getter expected_status, expected_json
+
+    def initialize(@expected_status : Int32, @expected_json : String)
     end
 
-    def match(response : HTTP::Client::Response)
-      response.status_code == @status && response.body == @expected_json
+    def match(actual_response : HTTP::Client::Response) : Bool
+      actual_response.status_code == expected_status &&
+        actual_response.body == expected_json
     end
 
-    def failure_message(response)
-      <<-TEXT
-    Response JSON does not match.
-
-    Expected:
-
-      Status: #{@status}
-      Body: #{@expected_json}
-
-    Actual:
-
-      Status: #{response.status_code}
-      Body: #{response.body}
-    TEXT
+    def failure_message(actual_response : HTTP::Client::Response) : String
+      if actual_response.status_code != expected_status
+        "Expected status of #{expected_status}. Instead got #{actual_response.status_code}."
+      else
+        incorrect_response_body_message(actual_response)
+      end
+    rescue JSON::ParseException
+      "Response body is not valid JSON."
     end
 
-    def negative_failure_message(actual_value)
+    private def incorrect_response_body_message(actual_response : HTTP::Client::Response) : String
+      actual_parsed_json = JSON.parse(actual_response.body)
+      expected_parsed_json = JSON.parse(expected_json)
+
+      result = expected_parsed_json.as_h.find do |key, value|
+        !actual_parsed_json.as_h.has_key?(key) || actual_parsed_json.as_h[key] != value
+      end
+
+      pp! result
+
+      "Boo!"
+    end
+
+    #   <<-TEXT
+    #   Expected response to have JSON key #{key}, but it was not present.
+
+    #   Got keys: #{actual_parsed_json.as_h.keys.join(", ")}
+    #   TEXT
+    #   break
+    # else
+    #   "No bueno"
+    # end
+
+    def negative_failure_message(actual_value) : String
       "Negative fail!"
     end
   end
